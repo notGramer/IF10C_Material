8351fbe54ef5b5094a33a80f6dc7f70db93f8d33
Ioannis Kiourtzidis <kiourtzidis.jiannis@gmail.com>
2024-10-07 10:17:39 +0200

    Material

---
+++ AEuP/LS02/PraktikantenCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ParktikantenCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl1, zahl2, ergebnis = 0.0;
+                                                                 //Variablen für Zahlen
+            char rechenzeichen;                                 //Variable für Rechenzeichen
+
+            Console.Write("Geben Sie die erste Zahl ein: ");    //Eingabeaufforderung ausgeben
+            zahl1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie das Rechenzeichen ein: "); //Eingabeaufforderung ausgeben
+            rechenzeichen = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Geben Sie die zweite Zahl ein: ");   //Eingabeaufforderung ausgeben
+            zahl2 = Convert.ToDouble(Console.ReadLine());       //Eingabe einlesen
+
+            if (rechenzeichen == '+')                             //je nach Operator berechnen
+                ergebnis = zahl1 + zahl2;
+            if (rechenzeichen == '-')
+                ergebnis = zahl1 - zahl2;
+            if (rechenzeichen == '*')
+                ergebnis = zahl1 * zahl2;
+            if (rechenzeichen == '/')
+                ergebnis = zahl1 / zahl2;
+
+            //Aufgabe noch mal komplett ausgeben
+            Console.WriteLine($"{zahl1} {rechenzeichen} {zahl2} = {ergebnis}");
+
+            Console.ReadKey();
+        }
+    }
+}
